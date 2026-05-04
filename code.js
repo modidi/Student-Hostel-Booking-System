9d00296491ff2368ebc8b3c6c7e46f0993fb0a4d
maureen <maureenndindamutua@gmail.com>
2026-05-04 20:32:05 +0300

    code.js: update on hostel data structure and update displayHostels function

--- code.js
+++ code.js
@@ -1,12 +1,71 @@
+//load from localStorage
 let hostels = JSON.parse(localStorage.getItem("hostels")) || [];
+
+// Hostel 1
+{
+    name: "Blooming Hostel",
+    location: "Nairobi CBD",
+    price: 5500,
+    description: "Affordable hostel near the city with good security.",
+
+    images: [
+        { src: "images/h1-1.jpg", label: "Hostel Exterior" },
+        { src: "images/h1-2.jpg", label: "Reception Area" },
+        { src: "images/h1-3.jpg", label: "Dorm Room (bunk beds)" },
+        { src: "images/h1-4.jpg", label: "Lounge Area" },
+        { src: "images/h1-5.jpg", label: "Study Area" },
+    ]
+
+};
+
+// Hostel 2
+{
+   name: "Olive View Hostel",
+   location: "Westlands",
+   price: "7500",
+   description: "Budget friendly Hostel with warm and social vibes.",
+
+   images: [
+    { src: "images/h2-1.jpg", label: "Hostel Exterior" },
+    { src: "images/h2-2.jpg", label: "Dorm Room Shared" },
+    { src: "images/h2-3.jpg", label: "Kitchen Area" },
+    { src: "images/h2-4.jpg", label: "Common social space" },
+    { src: "images/h2-5.jpg", label: "Hostel Entrance" },
+
+   ]
+
+};
+
+// Hostel 3
+{
+    name: "Elite Stay Hostel",
+    location: "Hurlingham",
+    price: 10000,
+    description: "Premium hostel with private rooms and study spaces.",
+
+    images: [
+        { src: "images/h3-1.jpg", label: "Luxury Exterior" },
+        { src: "images/h3-2.webp", label: "Private Room Interior" },
+        { src: "images/h3-3.webp", label: "Bathroom" },
+        { src: "images/h3-4.jpg", label: "Rooftop" },
+        { src: "images/h3-5.jpg", label: "Lounge" },
+    ]
+};
+
+
+// Bookings
 let bookings = JSON.parse(localStorage.getItem("bookings")) || [];
 
-// Save Data
-function saveHostels() {
-    localStorage.setItem("hostels", JSON.stringify(hostels));
+// Display Hostels
+function displayHostels() {
+
+    let list = document.getElementById("hostelList");
+    if (!list) return;
+
+    list.innerHTML = "";
+
+    
+
+
 
 }
-
-// Add Hostel
-let form = document.getElementById("hostelForm");
-
